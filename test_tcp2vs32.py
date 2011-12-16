f0799a7bbf6995c24b8fad86a5c71bd7aa26d21e
Vimalkumar <j.vimal@gmail.com>
2011-12-16 00:59:42 -0800

    Cleanup code and make a small change to make it IP address capable.

--- test_tcp2vs32.py
+++ test_tcp2vs32.py
@@ -1,6 +1,7 @@
 from expt import Expt
 from host import *
 from iperf import Iperf
+from time import sleep
 
 class Tcp2Vs32(Expt):
     def __init__(self, **kwargs):
@@ -11,53 +12,58 @@ class Tcp2Vs32(Expt):
         h1 = Host("10.0.1.1")
         h2 = Host("10.0.1.2")
         h3 = Host("10.0.1.3")
-        dev="eth2"
         self.hlist = HostList(h1, h2, h3)
         hlist = self.hlist
 
-        h1.prepare_iface()
-        h2.prepare_iface()
-        h3.prepare_iface()
-
+        hlist.prepare_iface()
+        hlist.configure_rps()
         hlist.rmmod()
         hlist.ipt_ebt_flush()
         if self.opts("enabled"):
             hlist.insmod()
             self.log("Creating two tenants")
-            h1.create_tcp_tenant(server_ports=[5001], tid=1)
-            h1.create_tcp_tenant(server_ports=[5002], tid=2)
+            #h1.create_tcp_tenant(server_ports=[5001], tid=1)
+            #h1.create_tcp_tenant(server_ports=[5002], tid=2)
+            #h2.create_tcp_tenant(server_ports=[5001], tid=1)
+            #h3.create_tcp_tenant(server_ports=[5002], tid=2)
+            h1.create_ip_tenant(tid=1)
+            h1.create_ip_tenant(tid=2)
 
-            h2.create_tcp_tenant(server_ports=[5001], tid=1)
-            h3.create_tcp_tenant(server_ports=[5002], tid=2)
+            h2.create_ip_tenant(tid=1)
+            h3.create_ip_tenant(tid=1)
 
         hlist.start_cpu_monitor()
         hlist.start_bw_monitor()
+        hlist.perfiso_set("ISO_VQ_DRAIN_RATE_MBPS", 8700)
+        hlist.perfiso_set("IsoAutoGenerateFeedback", 1)
+        hlist.perfiso_set("ISO_VQ_UPDATE_INTERVAL_US", 25)
 
         self.procs = []
         # Start iperf servers
         for p in [5001, 5002]:
-            iperf = Iperf({'-p': p,
-                           '-c': h1.get_10g_ip()})
-            server = iperf.start_server(h1.get_10g_ip())
+            iperf = Iperf({'-p': p})
+            server = iperf.start_server(h1.addr)
             self.procs.append(server)
 
+        sleep(1)
         # Start 1 TCP connection from h2 to h1
         client = Iperf({'-p': 5001,
-                        '-c': h1.get_10g_ip(),
+                        '-c': h1.get_tenant_ip(1), #h1.get_10g_ip(),
                         '-t': self.opts("t"),
                         '-P': 1})
-        client = client.start_client(h2.get_10g_ip())
+        client = client.start_client(h2.addr)
         self.procs.append(client)
 
         # Start 32 TCP from h3 to h1
         client = Iperf({'-p': 5002,
-                        '-c': h1.get_10g_ip(),
+                        '-c': h1.get_tenant_ip(2), #h1.get_10g_ip(),
                         '-t': self.opts("t"),
                         '-P': 32})
-        client = client.start_client(h3.get_10g_ip())
+        client = client.start_client(h3.addr)
         self.procs.append(client)
 
     def stop(self):
+        self.hlist.remove_tenants()
         for p in self.procs:
             p.kill()
         self.hlist.killall()
