e7f4332fd6331dad5c4c5b8c024018fcd9c9032f
Vimalkumar <j.vimal@gmail.com>
2011-12-23 11:20:32 -0800

    Add date to expt output path

--- rx-overhead.sh
+++ rx-overhead.sh
@@ -1,5 +1,6 @@
 
-dir=exptdata/rx
+dir=exptdata/`date +%b%d-%H:%M`/rx/
+time=120
 
 ctrlc() {
 	killall -9 python
@@ -10,8 +11,8 @@ trap ctrlc SIGINT
 
 for rate in 1000 3000 6000 9000; do
 	for n in 1 8 16 32 64 128; do
-		python tests/rx-overhead.py --rate $rate --dir $dir/r$rate-n$n -n $n -t 240
+		python tests/rx-overhead.py --rate $rate --dir $dir/r$rate-n$n -n $n -t $time
 		python tests/rx-overhead.py --rate $rate \
-			--dir $dir-without/r$rate-n$n -n $n -t 240 --without-vq
+			--dir $dir-without/r$rate-n$n -n $n -t $time --without-vq
 	done
 done
