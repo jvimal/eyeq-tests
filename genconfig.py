8300a7e133ceb5d2e585ed49dfd6221f0c6ca475
Vimalkumar <j.vimal@gmail.com>
2012-01-07 11:21:54 -0800

    Genconfig: more options
    Pretty much all options are now configurable.  fullmesh/incast traffic,
    size of bursts, repetition factor, inter-flow time, etc.  Run with
    --help for more info.

--- genconfig.py
+++ genconfig.py
@@ -30,7 +30,46 @@ parser.add_argument('-P',
                     help="Number of parallel connections per host",
                     default=4)
 
+parser.add_argument('--traffic',
+                    dest="traffic",
+                    choices=["fullmesh", "incast"],
+                    default="incast")
+
+parser.add_argument('--pattern',
+                    dest="pattern",
+                    choices=["onoff", "longlived"],
+                    default="onoff")
+
+parser.add_argument('--size',
+                    dest="size",
+                    default="100M")
+
+parser.add_argument('--tenant',
+                    dest="tenant",
+                    type=int,
+                    default=None)
+
+parser.add_argument('--repeat',
+                    dest="repeat",
+                    type=int,
+                    default=1000)
+
+parser.add_argument('--inter',
+                    dest="inter",
+                    type=int,
+                    default=10)
+
+
 args = parser.parse_args()
+
+def parse_size(s):
+    if 'K' in s:
+        size = int(s.replace('K','')) * 10**3
+    if 'M' in s:
+        size = int(s.replace('M', '')) * 10**6
+    elif 'G' in s:
+        size = int(s.replace('G', '')) * 10**9
+    return size
 
 # Simple full mesh
 def fullmesh():
@@ -40,22 +79,27 @@ def fullmesh():
     port = args.port
     seed = 0
     P = args.P
+    size = parse_size(args.size)
 
     print "#src_ip dst_ip dst_port type seed start_time stop_time flow_size r/e repetitions time_between_flows r/e (rpc_delay r/e)"
 
     for i in xrange(n):
         hi = pick_10g_ip(i)
+        if args.tenant:
+            hi = Host(pick_host_ip(i)).get_tenant_ip(args.tenant)
         for j in xrange(n):
             if i == j:
                 continue
             for p in xrange(P):
                 seed += 1
                 hj = pick_10g_ip(j)
+                if args.tenant:
+                    hj = Host(pick_host_ip(j)).get_tenant_ip(args.tenant)
                 out = "%s %s " % (hi, hj)
                 out += "%s %s %s " % (port, type, seed)
                 out += "%s %s " % (0.0, time)
-                out += "1000000000000 random "
-                out += "1000 0.001 random"
+                out += "%s exact " % size
+                out += "%s %s exact" % (args.repeat, args.inter)
                 print out
 
 def nto1():
@@ -64,20 +108,27 @@ def nto1():
     time = args.time
     port = args.port
     seed = 0
+    size = parse_size(args.size)
     P = args.P
     print "#src_ip dst_ip dst_port type seed start_time stop_time flow_size r/e repetitions time_between_flows r/e (rpc_delay r/e)"
 
     h0 = pick_10g_ip(0)
-    for i in xrange(1,n):
+    if args.tenant:
+        h0 = Host(pick_host_ip(0)).get_tenant_ip(args.tenant)
+    for i in xrange(2,n):
         hi = pick_10g_ip(i)
+        if args.tenant:
+            hi = Host(pick_host_ip(i)).get_tenant_ip(args.tenant)
         for p in xrange(P):
             seed += 1
             out = "%s %s " % (hi, h0)
             out += "%s %s %s " % (port, type, seed)
             out += "%s %s " % (0.0, time)
-            out += "100000000 random "
-            out += "1000 10 random"
+            out += "%s exact " % size
+            out += "%s %s exact" % (args.repeat, args.inter)
             print out
 
-fullmesh()
-#nto1()
+if args.traffic == "fullmesh":
+    fullmesh()
+elif args.traffic == "incast":
+    nto1()
