1d2fe99c9a07f026652e084ddca5fc43032c85b9
Vimalkumar <j.vimal@gmail.com>
2012-01-07 11:27:50 -0800

    Fix output directories in rx-overhead expt

--- rx-overhead.sh
+++ rx-overhead.sh
@@ -1,5 +1,5 @@
 
-dir=exptdata/`date +%b%d-%H:%M`/rx/
+dir=exptdata/`date +%b%d-%H:%M`/rx
 time=120
 
 mkdir -p $dir
@@ -13,7 +13,7 @@ trap ctrlc SIGINT
 
 for rate in 1000 3000 6000 9000; do
 	for n in 1 8 16 32 64 128; do
-		python tests/rx-overhead.py --rate $rate --dir $dir/r$rate-n$n -n $n -t $time
+		python tests/rx-overhead.py --rate $rate --dir $dir-with/r$rate-n$n -n $n -t $time
 		python tests/rx-overhead.py --rate $rate \
 			--dir $dir-without/r$rate-n$n -n $n -t $time --without-vq
 	done
