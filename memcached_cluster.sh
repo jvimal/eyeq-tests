e93c759174258aed4aa176b6d6ab346d3418d19b
Vimalkumar <j.vimal@gmail.com>
2012-01-09 22:45:41 -0800

    Streamline memcached cluster expt script
    Runs udp, mem in complete isolation, and then colocated.

--- memcached_cluster.sh
+++ memcached_cluster.sh
@@ -16,37 +16,44 @@ exptid=`date +%b%d-%H:%M`
 # With and Without isolation
 
 python tests/genconfig.py --traffic fullmesh \
-  --size $size --type udp --inter 10 -n 16 --time $time \
-  --tenant 2 > ~/vimal/exports/loadfiles/fullmesh_n16_udp_tenant_2_${size}
+	--size $size --type udp --inter 10 -n 16 --time $time \
+	-P 1 \
+	--tenant 2 > ~/vimal/exports/loadfiles/fullmesh_n16_udp_tenant_2_${size}
 
 python tests/genconfig.py --traffic fullmesh \
-  --size $size --type udp --inter 10 -n 16 --time $time \
-  > ~/vimal/exports/loadfiles/fullmesh_n16_udp_${size}
+	--size $size --type udp --inter 10 -n 16 --time $time \
+	-P 1 \
+	> ~/vimal/exports/loadfiles/fullmesh_n16_udp_${size}
+
+# Both tenants executed alone without interference
 
 for mtu in 9000; do
-  for iso in "" "--enable"; do
-    for workload in ~/vimal/exports/memcached_cluster/*; do
-		work=`basename $workload`
-		dir=/tmp/memcached-mtu$mtu-iso$iso-work$work
-		mkdir -p $dir
-		dirs="$dirs\n$dir"
+	for iso in "" "--enable"; do
+		for workload in ~/vimal/exports/memcached_cluster/*; do
+			for active in udp mem udp,mem; do
+				work=`basename $workload`
+				dir=/tmp/memcached-mtu$mtu-iso$iso-work$work-active$active
+				mkdir -p $dir
+				dirs="$dirs\n$dir"
 
-		traffic=fullmesh_n16_udp_${size}
-		if [[ $iso = "--enable" ]]; then
-			traffic=fullmesh_n16_udp_tenant_2_${size}
-		fi
+				traffic=fullmesh_n16_udp_${size}
+				if [[ $iso = "--enable" ]]; then
+					traffic=fullmesh_n16_udp_tenant_2_${size}
+				fi
 
-		echo $exptid $dir $workload $iso $mtu $traffic > $dir/README;
+				echo $exptid $dir $workload $iso $mtu $traffic $active > $dir/README;
 
-		python tests/test_memcached_cluster.py --ns 4 --nc 12 $iso \
-			--dir $dir \
-			--time $time \
-			--mtu $mtu \
-			--memaslap $workload \
-			--exptid $exptid \
-			--traffic ~/vimal/exports/loadfiles/$traffic
-    done
-  done
+				python tests/test_memcached_cluster.py --ns 4 --nc 12 $iso \
+					--dir $dir \
+					--time $time \
+					--mtu $mtu \
+					--memaslap $workload \
+					--exptid $exptid \
+					--traffic ~/vimal/exports/loadfiles/$traffic \
+					--active $active
+			done
+		done
+	done
 done
 
 echo `date` $exptid
