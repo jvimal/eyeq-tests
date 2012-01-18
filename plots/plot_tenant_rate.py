e8005cf692d8d3ad00a3c90d9b89f4166f56f9f5
Vimalkumar <j.vimal@gmail.com>
2012-01-17 16:39:33 -0800

    Plot tenant rate through loadgen output as well

--- plots/plot_tenant_rate.py
+++ plots/plot_tenant_rate.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from collections import defaultdict
 from helper import *
+import os
+import glob
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--files', '-f',
@@ -33,10 +36,45 @@ parser.add_argument('--title',
                     action="store",
                     dest="title")
 
+parser.add_argument('--accum',
+                    help="How many seconds to accumulate to compute average",
+                    default=None,
+                    type=int,
+                    action="store",
+                    dest="accum")
+
 m.rc('figure', figsize=(16, 6))
 args = parser.parse_args()
+LOADGEN_OUTPUT = 'loadgen'
+
+def parse_loadgen_file(f):
+    dir = os.path.dirname(f)
+    files = glob.glob(dir + "/%s*.txt" % LOADGEN_OUTPUT)
+    ret = {}
+    tid = -1
+    print files
+    for f in sorted(files):
+        tid += 1
+        lines = open(f).readlines()
+        for l in lines:
+            if l[0] not in '0123456789':
+                continue
+            try:
+                t, rx, tx, blah1, blah2 = map(float, l.strip().split(' '))
+            except:
+                print 'error, ignoring line:', f, l
+                continue
+            if not ret.has_key(tid):
+                ret[tid] = defaultdict(list)
+            ret[tid]['t'].append(t)
+            ret[tid]['rx'].append(rx)
+            ret[tid]['tx'].append(tx)
+        print 'parsed file %s' % f
+    return ret
 
 def parse_file(f):
+    if 'tenant.txt' not in f:
+        return parse_loadgen_file(f)
     lines = open(f).readlines()
     start_time = 0
     ret = {}
@@ -52,27 +90,53 @@ def parse_file(f):
         ret[tid]['t'].append(t - start_time)
         ret[tid]['tx'].append(tx)
         ret[tid]['rx'].append(rx)
+    print 'parsed file %s' % f
+    return ret
+
+def accum(values):
+    if args.accum is None or args.accum == 1:
+        return values
+    ret = []
+    count = 0
+    tot = 0
+    for v in values:
+        tot += v
+        count += 1
+        if count == args.accum:
+            ret.append(tot*1.0/args.accum)
+            count = 0
+            tot = 0
     return ret
 
 # Plots either tx/rx for all tenants on a particular server (one tenant.txt file)
 def plot_rate(ax, data, dir="tx", title="Rate"):
     total = []
+    TID = -1
+    default_colours = 'grb'
     for tid in sorted(data.keys()):
-        xvalues = data[tid]['t']
-        yvalues = data[tid][dir]
-        ax.plot(xvalues, yvalues, lw=2, label=tid)
+        #if tid != '11.0.1.1':
+        #    continue
+        TID += 1
+        xvalues = accum(data[tid]['t'])
+        yvalues = accum(data[tid][dir])
+        ax.plot(xvalues, yvalues, lw=2, label=str(tid), color=default_colours[TID])
         if len(total) == 0:
             total = yvalues
         else:
             total = map(sum, zip(total, yvalues))
 
     if len(total):
-        ax.plot(xvalues, total, lw=2, label="total")
-    ax.legend(loc='lower right')
+        l = min(len(total), len(xvalues))
+        ax.plot(xvalues[0:l], total[0:l], lw=2, label="total", color='b')
+    try:
+        ax.legend(loc='lower right')
+    except:
+        pass
     ax.set_title(title)
     ax.grid()
     ax.set_xlabel("Time (s)")
     ax.set_ylabel("Mbps")
+    ax.set_ylim((0, 10001))
     return
 
 for f in args.files:
