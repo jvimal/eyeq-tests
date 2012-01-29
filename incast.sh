61518d414dfc4dd1261ba5f80de0bc8809bda2f6
Vimalkumar <j.vimal@gmail.com>
2012-01-28 16:08:28 -0800

    Tweaks to incast experiment

--- incast.sh
+++ incast.sh
@@ -1,5 +1,5 @@
-
-dir=/tmp/`date +%b%d-%H:%M`/incast
+exptid=`date +%b%d-%H:%M`
+dir=/tmp/$exptid/incast
 time=120
 
 ctrlc() {
@@ -9,31 +9,34 @@ ctrlc() {
 
 trap ctrlc SIGINT
 
-basecmd="python tests/scenario.py --time $time -n 14 --run tcpvsudp"
+basecmd="python tests/scenario.py --time $time -n 14 --run tcpvsudp --exptid $exptid"
 
-for size in 10M 10G; do
-	for proto in tcp udp; do
-		for mtu in 1500 9000; do
+for iso in "" "--enabled"; do
+for size in 100G; do
+	for proto in udp; do
+		for mtu in 9000; do
             # WITH ISOLATION
 			python tests/genconfig.py --type $proto --traffic incast \
 				--size $size --repeat 1000 \
 				--inter 5 -n 16 --time 1000 \
+				--duration 100s \
 				--tenant 2 > ~/vimal/exports/14to1_${size}_${proto}_tenant
 
-			$basecmd --dir $dir/$proto-mtu$mtu-with --enable \
+			$basecmd --dir $dir/$proto-mtu$mtu-s$size-with$iso $iso \
 				--traffic ~/vimal/exports/14to1_${size}_${proto}_tenant \
-				--mtu $mtu
+				--mtu $mtu \
 
             # WITHOUT ISOLATION
-			python tests/genconfig.py --type $proto --traffic incast \
-				--size $size --repeat 1000 \
-				--inter 5 -n 16 --time 1000 > ~/vimal/exports/14to1_${size}_${proto}
+			#python tests/genconfig.py --type $proto --traffic incast \
+			#	--size $size --repeat 1000 \
+			#	--inter 5 -n 16 --time 1000 > ~/vimal/exports/14to1_${size}_${proto}
 
-			$basecmd --dir $dir/$proto-mtu$mtu-without \
-				--traffic ~/vimal/exports/14to1_${size}_${proto} \
-				--mtu $mtu
+			#$basecmd --dir $dir/$proto-mtu$mtu-s$size-without \
+			#	--traffic ~/vimal/exports/14to1_${size}_${proto} \
+			#	--mtu $mtu
 		done
 	done
+done
 done
 
 echo `date` $dir
