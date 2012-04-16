e2d0317fd3a2cfda93a67916dd78eea8bad333d3
Vimalkumar <j.vimal@gmail.com>
2012-04-16 10:11:43 -0700

    partition aggregate: plot output

--- partition-aggregate.sh
+++ partition-aggregate.sh
@@ -4,6 +4,8 @@ exptid=`date +%b%d-%H:%M`
 time=120
 start=`date`
 ntenant=2
+cpubind=""
+# cpubind="--bind"
 
 ctrlc() {
 	killall -9 python
@@ -16,12 +18,39 @@ echo exptid $exptid
 
 for size in 10K 100K 1M; do
 for iso in "" "--enable"; do
-	subdir=size$size-iso$iso
+	subdir=paggr-size$size-iso$iso
 	python tests/test_partition_aggregate.py -n $ntenant \
 		--size $size \
-		$iso
+		--dir /tmp/$exptid/$subdir \
+		--exptid $exptid \
+		--repeat 30000 \
+		$iso $cpubind \
 done
 done
 
+pushd ../exptdata/$exptid
+for dir in *; do
+for ext in pdf png; do
+	mkdir -p graphs/$ext
+
+	python2.6 ~/iso/tests/plots/plot-paggr-xput-latency.py --bin 1e-6 \
+		--title "Partition Aggregate ($dir)" \
+		-o graphs/$ext/$dir.$ext \
+		--dir $dir
+
+	python2.6 ~/iso/tests/plots/plot_rate.py \
+		-f $dir/l1/net.txt \
+		-o graphs/$ext/txrate.$ext
+
+	python2.6 ~/iso/tests/plots/plot_rate.py \
+		-f $dir/l1/net.txt \
+		-o graphs/$ext/rxrate.$ext \
+		--rx
+
+done
+done
+popd
+
 echo $start $exptid
 echo `date`
+
