0164aa54468ef24aa0483bb2c0611a659cb920fb
Vimalkumar <j.vimal@gmail.com>
2012-04-17 12:10:07 -0700

    plot some graphs for memcached cluster expts.

--- memcached_cluster.sh
+++ memcached_cluster.sh
@@ -16,12 +16,12 @@ exptid=`date +%b%d-%H:%M`
 # With and Without isolation
 
 python tests/genconfig.py --traffic fullmesh \
-	--size $size --type udp --inter 10 -n 16 --time $time \
+	--size $size --type udp --inter 16 -n 16 --time $time \
 	-P 1 \
 	--tenant 2 > ~/vimal/exports/loadfiles/fullmesh_n16_udp_tenant_2_${size}
 
 python tests/genconfig.py --traffic fullmesh \
-	--size $size --type udp --inter 10 -n 16 --time $time \
+	--size $size --type udp --inter 16 -n 16 --time $time \
 	-P 1 \
 	> ~/vimal/exports/loadfiles/fullmesh_n16_udp_${size}
 
@@ -50,11 +50,39 @@ for mtu in 9000; do
 					--memaslap $workload \
 					--exptid $exptid \
 					--traffic ~/vimal/exports/loadfiles/$traffic \
+          --nconn 16 \
 					--active $active
 			done
 		done
 	done
 done
 
+pushd ../exptdata/$exptid
+for workload in ~/vimal/exports/memcached_cluster/*; do
+for ext in eps png pdf; do
+    work=`basename $workload`;
+    echo $work
+    mkdir -p graphs/$ext;
+    python2.6 ~/iso/tests/plots/plot-memcached-stats.py -f \
+        memcached-mtu9000-iso--enable-work$work-activemem/l5/memaslap.txt \
+        memcached-mtu9000-iso--enable-work$work-activeudp,mem/l5/memaslap.txt \
+        memcached-mtu9000-iso-work$work-activeudp,mem/l5/memaslap.txt \
+        --legend mem udp,mem+EyeQ udp,mem-without-EyeQ \
+        -o graphs/$ext/$work.$ext;
+
+    # RX/TX rates for one server/client for mem and mem,udp active tenants
+    for tenants in mem udp,mem; do
+      python2.6 ~/iso/tests/plots/plot_tenant_rate.py \
+          --files memcached-mtu9000-iso--enable-work$work-active$tenants/l1/tenant.txt \
+          -o graphs/$ext/rate$workload-server-$tenants.$ext -l mem udp
+
+      python2.6 ~/iso/tests/plots/plot_tenant_rate.py \
+          --files memcached-mtu9000-iso--enable-work$work-active$tenants/l5/tenant.txt \
+          -o graphs/$ext/rate$workload-client-$tenants.$ext -l mem udp
+    done
+done
+done
+
 echo `date` $exptid
 echo -e $dirs
+
