32bbaf30b3b4180c9540a6a02b7c8f4caa69f43f
Vimalkumar <j.vimal@gmail.com>
2012-05-01 22:54:39 -0700

    few feature additions: plot png, pdf; reduce graphs to just two cases

--- hadoop-multi.sh
+++ hadoop-multi.sh
@@ -15,10 +15,10 @@ echo exptid $exptid
 
 for mtu in 9000; do
 for size in 10T; do
-for iso in "" "--enable" "--weighted" "--inv-weighted"; do
+for iso in "" "--inv-weighted"; do # other valid options "--enable" and "--weighted"
 for nhadoop in 3; do
 	subdir=size$size-iso$iso-mtu$mtu-nhadoop$nhadoop
-	python tests/test_hadoop_trace.py --mtu 9000 \
+	python tests/test_hadoop_trace.py --mtu $mtu \
 		--dir /tmp/$exptid/$subdir --size $size \
 		--exptid $exptid $iso --nhadoop $nhadoop
 done
@@ -29,10 +29,15 @@ done
 # Plot the output
 pushd ~/vimal/10g/exptdata/$exptid
 for dir in *; do
-	python2.6 ~/iso/tests/plots/plot-hadoop-trace-tenants.py --dirs $dir --out $dir.png
-	python2.6 ~/iso/tests/plots/plot-hadoop-trace-tenants.py --dirs $dir --out $dir.eps
+for ext in png pdf; do
+	python2.6 ~/iso/tests/plots/plot-hadoop-trace-tenants.py --dirs $dir \
+		--out $dir.$ext \
+		--maxx 2500
+done
 done
 popd
 
 echo $start $exptid
+echo hadoop-multi $exptid >> TODO
+
 echo `date`
