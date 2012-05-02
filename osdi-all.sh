b103f3d91904111102ed12b20ab472211dc4721d
Vimalkumar <j.vimal@gmail.com>
2012-05-01 22:58:14 -0700

    Reference script for final set of OSDI graphs

---
+++ osdi-all.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Invoking OSDI experiments
+
+###########################################
+# Micro evaluations
+###########################################
+
+function micro_overhead {
+	bash tests/overhead.sh
+}
+
+function micro_convergence {
+	bash tests/incast.sh
+}
+
+# Micro: bursty (TODO)
+function micro_bursty {
+	bash tests/incast-onoff.sh
+}
+
+
+###########################################
+# Macro evaluations
+###########################################
+
+function macro_multihadoop {
+	bash tests/hadoop-multi.sh
+}
+
+function macro_memcached_high {
+	bash tests/memcached_cluster_onoff.sh 6000
+}
+
+function macro_memcached_low {
+	bash tests/memcached_cluster_onoff.sh 3000
+}
+
+function macro_xen_latency {
+	# TODO
+	echo TODO
+}
+
+macro_multihadoop
+macro_memcached_high
+macro_memcached_low
+for i in {1..3}; do
+	micro_overhead
+done
