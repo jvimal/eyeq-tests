4f55221976edc601a2fba6603aa6f419dac7bcb1
Vimalkumar <j.vimal@gmail.com>
2012-05-03 13:13:37 -0700

    two vs 32 tcp flows... maybe do it with Xen??

--- test_tcp2vs32.py
+++ test_tcp2vs32.py
@@ -2,12 +2,41 @@ from expt import Expt
 from host import *
 from iperf import Iperf
 from time import sleep
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dir',
+                        required=True,
+                        dest="dir")
+
+    parser.add_argument('--enable', '--enabled',
+                        action="store_true",
+                        dest="enabled",
+                        help="Enable perfisolation",
+                        default=False)
+
+    parser.add_argument('--time', '-t',
+                        type=int,
+                        dest="time",
+                        help="Time to run expt",
+                        default=120)
+
+    parser.add_argument('--wtcp',
+                        dest="wtcp",
+                        type=int,
+                        help="Weight of the TCP flow.",
+                        default=2)
+
+    parser.add_argument('--vqrate',
+                        dest="vqrate",
+                        help="VQ drain rate.",
+                        default="9000")
+
+    args = parser.parse_args()
 
 class Tcp2Vs32(Expt):
-    def __init__(self, **kwargs):
-        Expt.__init__(self, kwargs)
-        self.desc = """Test fairness between 1 TCP and 32 TCP connections"""
-
     def start(self):
         h1 = Host("10.0.1.1")
         h2 = Host("10.0.1.2")
@@ -18,7 +47,6 @@ class Tcp2Vs32(Expt):
         hlist.prepare_iface()
         hlist.configure_rps()
         hlist.rmmod()
-        hlist.ipt_ebt_flush()
         if self.opts("enabled"):
             hlist.insmod()
             self.log("Creating two tenants")
@@ -26,17 +54,17 @@ class Tcp2Vs32(Expt):
             #h1.create_tcp_tenant(server_ports=[5002], tid=2)
             #h2.create_tcp_tenant(server_ports=[5001], tid=1)
             #h3.create_tcp_tenant(server_ports=[5002], tid=2)
-            h1.create_ip_tenant(tid=1)
+            h1.create_ip_tenant(tid=1, weight=self.opts("wtcp"))
             h1.create_ip_tenant(tid=2)
 
             h2.create_ip_tenant(tid=1)
             h3.create_ip_tenant(tid=1)
 
         if self.opts("enabled"):
-            hlist.perfiso_set("ISO_VQ_DRAIN_RATE_MBPS", 8700)
+            hlist.perfiso_set("ISO_VQ_DRAIN_RATE_MBPS", self.opts("vqrate"))
             hlist.perfiso_set("IsoAutoGenerateFeedback", 1)
             hlist.perfiso_set("ISO_VQ_UPDATE_INTERVAL_US", 25)
-        hlist.start_monitors(self.opts("dir"))
+        hlist.start_monitors(self.opts("dir"), 1e3)
 
         self.procs = []
         # Start iperf servers
@@ -72,3 +100,7 @@ class Tcp2Vs32(Expt):
         for p in self.procs:
             p.kill()
         self.hlist.killall()
+
+
+if __name__ == "__main__":
+    Tcp2Vs32(vars(args)).run()
