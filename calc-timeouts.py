c4da8c8cbd6f579195cdf127f6e149c9b8dc5ec6
Vimalkumar <j.vimal@gmail.com>
2012-05-03 13:12:09 -0700

    Aggregate timeouts

--- calc-timeouts.py
+++ calc-timeouts.py
@@ -5,8 +5,15 @@ import os
 parser = argparse.ArgumentParser(description="Calculate timeouts during experiments.")
 parser.add_argument('--dir', '-d',
                     dest="dir",
+                    nargs="+",
                     help="Directory containing netstat_{begin,end}.txt files",
                     required=True)
+
+parser.add_argument('-a',
+                    dest="aggr",
+                    help="sum all timeouts",
+                    action="store_true",
+                    default=False)
 
 args = parser.parse_args()
 
@@ -18,8 +25,16 @@ def parse_file(f):
             ret[desc] = int(num)
     return ret
 
-begin = parse_file(os.path.join(args.dir, "netstat_begin.txt"))
-end = parse_file(os.path.join(args.dir, "netstat_end.txt"))
+total = 0
+for dir in args.dir:
+    begin = parse_file(os.path.join(dir, "netstat_begin.txt"))
+    end = parse_file(os.path.join(dir, "netstat_end.txt"))
 
-for k in begin.keys():
-    print "%10d    %s" % (end[k] - begin[k], k)
+    for k in begin.keys():
+        timeouts = end[k] - begin[k]
+        if not args.aggr:
+            print "%10d    %s" % (timeouts, k)
+        else:
+            total += timeouts
+    print dir, total
+print 'total', total
