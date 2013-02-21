89fb6f36389fab598a3991e03efda51d31df6587
Vimalkumar <j.vimal@gmail.com>
2013-02-21 10:51:14 -0800

    preconfigure EyeQ in Mininet with params for 100Mb/s

--- mininet/100mbps.sh
+++ mininet/100mbps.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Set the interface speed to 100Mb/s
-tools/pi.py --set 18,100
+dir=${1:-.}
 
-# Set the receive speed to 100Mb/s
-tools/pi.py --set 5,100
+pushd $dir
+
+# Set the TX speed to 100Mb/s
+tools/pi.py --set 18,98
+
+# Set the RX speed to 92Mb/s
+tools/pi.py --set 5,92
 
 # Set the tokenbucket timeout value to 1ms
 tools/pi.py --set 3,1000000
@@ -13,8 +17,9 @@ tools/pi.py --set 3,1000000
 tools/pi.py --set 13,10000
 
 # Trigger a recompute on every host
-num_hosts=3
+num_hosts=4
 for i in `seq 1 $num_hosts`; do
-        echo "dev h$i-eth0 11.0.1.$i weight 1" > /sys/module/perfiso/parameters/set_txc_weight
-        echo "dev h$i-eth0 11.0.1.$i weight 1" > /sys/module/perfiso/parameters/set_vq_weight
+    echo "dev h$i-eth0" > /sys/module/perfiso/parameters/recompute_dev
 done
+
+popd
--- mininet/simple.py
+++ mininet/simple.py
@@ -17,17 +17,26 @@ import sys
 import os
 
 parser = argparse.ArgumentParser(description="EyeQ test topology in Mininet")
+parser.add_argument('--num-hosts',
+                    type=int,
+                    default=4)
+parser.add_argument('--num-tenants',
+                    type=int,
+                    help="Number of tenants in every host",
+                    default=3)
 args = parser.parse_args()
 
 lg.setLogLevel('info')
 
-def cmd(h, c):
-    print c
+def cmd(h, c, **kwargs):
+    if not kwargs.get("quiet", False):
+        print c
     h.sendCmd(c)
     h.waitOutput()
 
-def rootcmd(c):
-    print c
+def rootcmd(c, **kwargs):
+    if not kwargs.get("quiet", False):
+        print c
     Popen(c, shell=True).wait()
 
 class StarTopo(Topo):
@@ -44,18 +53,42 @@ class StarTopo(Topo):
         return
 
 def ssh_init(net):
+    lg.info("--- Starting sshd inside all hosts\n")
     for host in net.hosts:
-        cmd(host, "/usr/sbin/sshd")
+        cmd(host, "/usr/sbin/sshd", quiet=True)
     return
 
-def eyeq_conf():
+def eyeq_conf(net):
+    dir = os.path.dirname(os.path.abspath(__file__)) + "/"
+    basedir = dir + "../../"
+    testdir = dir + "../"
+
+    lg.info("--- Please wait while we create+configure tenants through ssh.\n")
+    lg.info("    This can take a while...\n")
+    h1 = net.get("h1")
+    cmd(h1, "python %s/tenant.py -m %d -T %d" % (testdir, args.num_hosts, args.num_tenants))
+
+    lg.info("\n--- Setting parameters for 100Mb/s\n")
+    rootcmd("bash %s/100mbps.sh %s" % (dir, basedir))
+
+def set_switch_rates(net):
+    lg.info("--- Adding rate limits to switches.\n")
+    for sw in net.switches:
+        for intf in sw.intfs.itervalues():
+            dev = intf.name
+            if dev == "lo":
+                continue
+            c = "tc qdisc add dev %s root handle 1: tbf limit 150000 burst 15000 rate 100Mbit" % dev
+            rootcmd(c)
     return
 
 def main():
-    topo = StarTopo(n=4)
+    topo = StarTopo(n=args.num_hosts)
     net = Mininet(topo=topo)
     net.start()
     ssh_init(net)
+    eyeq_conf(net)
+    set_switch_rates(net)
     CLI(net)
     net.stop()
     return
