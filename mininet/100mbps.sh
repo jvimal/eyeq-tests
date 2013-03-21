c00a45df72f7fff05d553aa99e176929b09c525f
Vimalkumar <j.vimal@gmail.com>
2013-03-21 10:21:47 -0700

    Better defaults for 100Mb/s.

--- mininet/100mbps.sh
+++ mininet/100mbps.sh
@@ -7,17 +7,17 @@ pushd $dir
 # Set the TX speed to 100Mb/s
 tools/pi.py --set 18,98
 
-# Set the RX speed to 92Mb/s
-tools/pi.py --set 5,92
+# Set the RX speed to 98Mb/s
+tools/pi.py --set 5,98
 
-# Set the tokenbucket timeout value to 1ms
-tools/pi.py --set 3,1000000
+# Set the tokenbucket timeout value to 100us
+tools/pi.py --set 3,100000
 
-# Set the rate metering interval to 10ms
-tools/pi.py --set 13,10000
+# Set the rate metering interval to 5ms
+tools/pi.py --set 13,5000
 
 # Trigger a recompute on every host
-num_hosts=4
+num_hosts=8
 for i in `seq 1 $num_hosts`; do
     echo "dev h$i-eth0" > /sys/module/perfiso/parameters/recompute_dev
 done
